f6cab93d40a5928d5f858a3ce80e186867e6fff2
Stephen Higgs <s.j.higgs@gmail.com>
2025-07-17 09:24:57 -0400

    minor update -- removed commented scram configs

--- infra/kafka/rhel/scripts/init-kraft-tls-sasl.sh
+++ infra/kafka/rhel/scripts/init-kraft-tls-sasl.sh
@@ -11,10 +11,10 @@ eval rm -rf /tmp/kraft-controller-logs-{0..5}
 eval rm -rf /tmp/kraft-combined-logs-{0..5}
 
 $KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --standalone --config ../config/kraft-tls-sasl/controller-0.properties --add-scram 'SCRAM-SHA-512=[name=admin,password=secret]'
-$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/controller-1.properties #--add-scram 'SCRAM-SHA-512=[name=admin,password=secret]'
-$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/controller-2.properties #--add-scram 'SCRAM-SHA-512=[name=admin,password=secret]'
+$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/controller-1.properties
+$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/controller-2.properties
 
-$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/broker-3.properties #--add-scram 'SCRAM-SHA-512=[name=admin,password=secret]'
-$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/broker-4.properties #--add-scram 'SCRAM-SHA-512=[name=admin,password=secret]'
-$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/broker-5.properties #--add-scram 'SCRAM-SHA-512=[name=admin,password=secret]'
+$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/broker-3.properties
+$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/broker-4.properties
+$KAFKA_DIR/bin/kafka-storage.sh format --cluster-id q6Wklx3BQF6RxKTRElZYUA --no-initial-controllers --config ../config/kraft-tls-sasl/broker-5.properties
 
