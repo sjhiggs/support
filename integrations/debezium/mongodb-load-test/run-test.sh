a8783bdcc86ca7cc968a7d872fc462559151196b
Stephen Higgs <s.j.higgs@gmail.com>
2025-10-01 12:00:00 -0400

    update for sharded config

--- integrations/debezium/mongodb-load-test/run-test.sh
+++ integrations/debezium/mongodb-load-test/run-test.sh
@@ -4,5 +4,5 @@
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @my.json
 
 # run the load test on the mongo pod
-podman cp load_test.js mongo1:load_test.js 
-podman exec -i mongo1 mongosh "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=rs0" --file load_test.js
+podman cp load_test.js mongo-shard1-1:load_test.js 
+podman exec -i mongo-shard1-1 mongosh "mongodb://mongo-router:27017" --file load_test.js
