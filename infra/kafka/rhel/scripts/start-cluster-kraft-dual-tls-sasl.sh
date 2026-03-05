52d93712d4173d6cc1934718c8cf05e7b5e5e275
Stephen Higgs <s.j.higgs@gmail.com>
2026-03-05 14:30:34 -0500

    reorganize kafka rhel examples; they will likely still need mods for relative directories etc.

--- infra/kafka/rhel/scripts/start-cluster-kraft-dual-tls-sasl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-export KAFKA_DIR=/tmp/kafka-3.9.0
-
-LOG_DIR=/tmp/kafka-0/logs $KAFKA_DIR/bin/kafka-server-start.sh -daemon ../config/kraft-dual-tls-sasl/server-0.properties
-LOG_DIR=/tmp/kafka-1/logs $KAFKA_DIR/bin/kafka-server-start.sh -daemon ../config/kraft-dual-tls-sasl/server-1.properties
-LOG_DIR=/tmp/kafka-2/logs $KAFKA_DIR/bin/kafka-server-start.sh -daemon ../config/kraft-dual-tls-sasl/server-2.properties
